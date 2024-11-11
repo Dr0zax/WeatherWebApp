3fe2624080d387aa8e6024b69cc37961692a7ac5
Dr0zax <geodashfan4@gmail.com>
2024-11-11 08:59:18 -0700

    fixed bug with gradient code

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,6 +1,6 @@
 import { get3HourForecast } from "./weather-api/get3hourforcast.mjs";
 
-let weather = await get3HourForecast()
+// let weather = await get3HourForecast()
 
 const currentWeather = document.querySelector(".current-weather")
 const units = document.getElementById("units")
@@ -30,13 +30,17 @@ const setGradient = () => {
     let time = new Date();
     let hours = time.getHours();
     
-    if (hours < 7) {
-        currentWeather.style.background = "linear-gradient(0deg, rgb(105, 65, 65), rgb(60, 167, 255))";
+    if (hours < 7 && hours >= 5) {
+        //Sunrise
+        currentWeather.style.background = "linear-gradient(0deg, rgb(218, 136, 136), rgb(45, 129, 199))";
     } else if (hours >= 7 && hours < 18) {
+        //Day
         currentWeather.style.background = "linear-gradient(0deg, rgb(60, 167, 255), rgb(132, 200, 255))";
     } else if (hours >= 18 && hours < 19) {
+        //Sunset
         currentWeather.style.background = "linear-gradient(0deg, rgb(105, 65, 65), rgb(66, 82, 163))";
-    } else  {
+    } else {
+        //Night
         currentWeather.style.background = "linear-gradient(0deg, rgb(24, 37, 104), rgb(0, 0, 0))";
     }
         
