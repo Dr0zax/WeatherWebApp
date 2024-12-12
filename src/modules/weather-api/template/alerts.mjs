012d845da151a9445d78964080b0a4c9ccaf9ebd
Everest Wilkey <131012192+Everestwilkey@users.noreply.github.com>
2024-12-12 10:40:02 -0700

    fixed css

--- src/modules/weather-api/template/alerts.mjs
+++ src/modules/weather-api/template/alerts.mjs
@@ -1,7 +1,7 @@
 export default function createAlertHTML(feature) {
     return `
         <div class="alert">
-            <section class="severity ${(feature.properties.severity || "Unknown").toLowerCase()}">
+            <section class="severity${(feature.properties.severity || "Unknown")}">
                 <h1><span id="severity">${feature.properties.severity || "Unknown"}</span> Alert</h1>
             </section>
             <section class="alert-content">
